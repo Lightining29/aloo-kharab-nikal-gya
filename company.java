63a280b892fefd232a21f637fb73ec029ef7e7b0
Manish kumar <brayw433@gmail.com>
2025-04-05 12:20:24 +0530

    added implementation for company.java

--- company.java
+++ company.java
@@ -28,13 +28,13 @@ class Employee {
     }
 
     public void displayEmployee() {
-        System.out.println("name:" + name);
+        System.out.println("naam:" + name);
         System.out.println("employee id : " + employeeid);
-        System.out.println("your basic salary : " + basicsalary);
+        System.out.println("aapki salary : " + basicsalary);
         System.out.println("your  hra is :" + hra);
         System.out.println("your da is :" + da);
         System.out.println("your travelling allowance is :" + travelallowance);
-        System.out.println("your taotal salary : " + totalsalary);
+        System.out.println("apni total salary btao : " + totalsalary);
 
     }
 
@@ -46,9 +46,9 @@ public class company {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter employee name");
         String name = sc.nextLine();
-        System.out.println("enter employee id ");
+        System.out.println("apni id likho ");
         int employeeid = sc.nextInt();
-        System.out.println("enter basic salary");
+        System.out.println("apni salary likho");
         double basicsalary = sc.nextDouble();
         Employee ob = new Employee();
         ob.employee(name, employeeid, basicsalary);
